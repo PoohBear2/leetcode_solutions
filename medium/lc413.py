a57d95672648b38d168f242675241cf12a7c0752
PoohBear2 <jzhou3379@gmail.com>
2026-04-21 14:04:16 -0400

    lc413 4/21

---
+++ medium/lc413.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        if n <= 1:
+            return 0
+        
+        dp = [0 for _ in range(n)]
+
+        for i in range(2, n):
+            if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:
+                dp[i] = dp[i - 1] + 1
+        
+        return sum(dp)
